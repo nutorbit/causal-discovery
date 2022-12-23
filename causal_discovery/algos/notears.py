56fcf8329394eca5ba7133cd97e9b10261881569
nutorbit <nutorbitx@gmail.com>
2022-12-23 15:09:33 +0700

    add notears algorithm

---
+++ causal_discovery/algos/notears.py
@@ -0,0 +1,105 @@
+import jax
+import optax
+import jax.numpy as jnp
+
+from typing import List
+from tqdm import trange
+
+
+class NoTears:
+    """
+    An implementation of the NoTEARS algorithm for causal discovery using gradient descent.
+
+    Paper: https://arxiv.org/abs/1803.01422
+    """
+    # TODO: need to refactor
+    
+    def __init__(self, rho: float, alpha: float, l1_reg: float, lr: float=1e-3):
+        self.rho = rho
+        self.alpha = alpha
+        self.l1_reg = l1_reg
+        self.opt = optax.adam(lr)
+        self.W = None
+        
+        self.loss_fn = jax.jit(self.loss_fn)
+        
+    def init_params(self, n: int) -> jnp.ndarray:
+        """
+        Initialize the parameters.
+        
+        Args:
+            n: the number of features.
+        """
+        
+        return jnp.zeros((n, n))
+    
+    def h(self, W: jnp.ndarray) -> jnp.ndarray:
+        """
+        Calculate the h(W) term in (16).
+        
+        Args:
+            W: the weight matrix.
+        """
+        
+        return jnp.trace(jax.scipy.linalg.expm(W * W)) - W.shape[0]
+    
+    def loss_fn(self, W: jnp.ndarray, X: jnp.ndarray, rho: float, alpha: float) -> jnp.ndarray:
+        """
+        Calculate the loss (ECP) according to (15), (16).
+        
+        Args:
+            W: the weight matrix.
+            X: the data matrix.
+            rho: the regularization parameter.
+            alpha: the regularization parameter.
+            
+        Returns:
+            ECP loss
+        """
+        
+        n, d = X.shape
+        
+        W = W - jnp.diag(jnp.diag(W))
+        
+        mse = 0.5 / n * jnp.square(jnp.linalg.norm(X - X @ W))
+        reg = self.l1_reg * jnp.linalg.norm(W, ord=1)
+        h = self.h(W)
+        rho_term = 0.5 * rho * h * h
+        alpha_term = alpha * h
+        
+        return mse + reg + rho_term + alpha_term
+    
+    def learn(self, X: jnp.ndarray, n_outer_iter: int=20, n_inner_iter: int=100) -> List[float]:
+        """
+        Learn the weight matrix.
+        
+        Args:
+            X: the data matrix.
+            n_outer_iter: the number of outer iterations.
+            n_inner_iter: the number of inner iterations.
+        """
+        
+        n = X.shape[1]
+        self.W = self.init_params(n)
+        opt_state = self.opt.init(self.W)
+        loss_history = []
+        h = jnp.inf
+        
+        for _ in trange(n_outer_iter):
+            while self.rho < 1e6:
+                for _ in range(n_inner_iter):
+                    loss, grad = jax.value_and_grad(self.loss_fn)(self.W, X, self.rho, self.alpha)
+                    updates, opt_state = self.opt.update(grad, opt_state)
+                    self.W = optax.apply_updates(self.W, updates)
+                    loss_history.append(float(loss))
+
+                new_h = self.h(self.W)                
+                if new_h > 0.25 * h:
+                    self.rho *= 10
+                else:
+                    break
+                
+            h = new_h
+            self.alpha += self.rho * h
+        
+        return loss_history
---
+++ causal_discovery/scripts/run_notears.py
@@ -0,0 +1,35 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from causal_discovery.algos.notears import NoTears
+from causal_discovery.synthetic_dataset import SyntheticDataset
+
+
+def main():
+    n, d = 1000, 20
+    graph_type, degree, sem_type = 'erdos-renyi', 4, 'linear-gauss'
+    noise_scale = 1.0
+
+    dataset = SyntheticDataset(n, d, graph_type, degree, sem_type,
+                               noise_scale, dataset_type='linear')
+    
+    model = NoTears(
+        rho=1, 
+        alpha=0.1, 
+        l1_reg=0, 
+        lr=1e-2
+    )
+
+    loss_hist = model.learn(dataset.X)
+    
+    plt.plot(loss_hist)
+    plt.show()
+    
+    fig, ax = plt.subplots(ncols=2)
+    sns.heatmap(dataset.W, ax=ax[0])
+    sns.heatmap(model.W, ax=ax[1])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
